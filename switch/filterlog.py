cd3a7a1db4949963e11096d60b011d658b723630
Theo <theojepsen@g-m-a-i-l-dotcom>
2016-10-04 21:26:19 +0200

    Script for reading gotthard client/server logs

---
+++ switch/filterlog.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import json
+import hashlib
+import argparse
+import sys
+from pygotthard import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("log", type=str, help="JSON log filename", nargs='?')
+parser.add_argument('--checksum', '-c', help="print value checksum", action='store_true', default=False)
+parser.add_argument('--tsv', '-t', help="output tsv data", action='store_true', default=False)
+args = parser.parse_args()
+
+tsv_fields = ['msg_type', 'cl_id', 'req_id', 'frag_seq', 'frag_cnt', 'op_cnt', 'rb_cnt', 'r_cnt', 'w_cnt']
+
+def parseFile(f):
+    for line in f.readlines():
+        line = unicode(line, errors='replace')
+        e = json.loads(line)
+        #e = json.loads(line, encoding='ascii')
+        if 'req' not in e and 'res' not in e: continue
+        msg_type = 'req' if 'req' in e else 'res'
+        m = e[msg_type]
+
+        if args.checksum:
+            for o in m['ops']:
+                o['v'] = hashlib.md5(o['v'].encode('utf-8')).hexdigest()[:4]
+
+        if args.tsv:
+            extra = dict(msg_type=msg_type,
+                    rb_cnt=len([o for o in m['ops'] if o['t'] == 'V']),
+                    w_cnt=len([o for o in m['ops'] if o['t'] == 'W']),
+                    r_cnt=len([o for o in m['ops'] if o['t'] == 'R']),
+                    op_cnt=len(m['ops']))
+            row = map(str, [m[k] if k in m else extra[k] for k in tsv_fields])
+            print '\t'.join(row)
+        else:
+            print json.dumps(e)
+
+if args.tsv:
+    print '\t'.join(tsv_fields)
+
+if not args.log or args.log == '-':
+    parseFile(sys.stdin)
+else:
+    with open(args.log, 'r') as f:
+        parseFile(f)
+
