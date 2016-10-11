8d02c5bb40b5cab985ecdc4bd991bc773ac79f13
Theo <theojepsen@g-m-a-i-l-dotcom>
2016-10-11 16:17:23 +0200

    Added script to run generated experiments

--- switch/scripts/make_read_experiments.sh
+++ switch/scripts/make_read_experiments.sh
@@ -6,21 +6,28 @@ else
     EXPERIMENTS_DIR=$1
 fi
 
-for DR in 0.01 0.1 0.5 0.75
+DONE_DIR="$EXPERIMENTS_DIR/done"
+TORUN_DIR="$EXPERIMENTS_DIR/torun"
+mkdir $DONE_DIR
+mkdir $TORUN_DIR
+
+for DR in .01 .1 .2 .3 .4 .5 .9
 do
     for TD in 50
+    #for TD in 100
     do
-        for N in 2 3 4
+        #for N in 2 3 4
+        for N in 4 6 8
         do
-            for write_p in 0.05 0.1 0.3 0.5 0.7 0.9
+            for write_p in 0 .1 .3 .5 .7 .9 1
             do
                 for switch_mode in 'read_cache' 'forward' 'early_abort' 'optimistic_abort'
                 do
                     python gen_experiment.py --stdout-log --sequential-clients \
-                        -c  "../pygotthard/load_generator.py -n %num_clients% -c %txn_cnt% -T A(1,a)W(1,RND)|R(1) -p %write_p% -l %l %h %p" \
+                        -c  "../pygotthard/load_generator.py -n %num_clients% -d %runtime% -T A(x,x)W(x,RND)|R(x) -p %write_p% -l %l %h %p" \
                         --server-cmd  "../pygotthard/store.py -l %l -p %p" \
-                        --out-parent $EXPERIMENTS_DIR --mode $switch_mode \
-                        -p num_clients=$N -p txn_cnt=1000 -p write_p=$write_p \
+                        --out-parent $TORUN_DIR --mode $switch_mode \
+                        -p num_clients=$N -p runtime=180 -p write_p=$write_p \
                         --total-delta $TD --delta-ratio $DR
                 done
             done
--- switch/scripts/make_tppc_experiments.sh
+++ switch/scripts/make_tppc_experiments.sh
@@ -6,19 +6,19 @@ else
     EXPERIMENTS_DIR=$1
 fi
 
-for DR in 0.01 0.1 0.5 0.75 0.9
+for DR in 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.75 0.9
 do
     for TD in 50 100
+    #for TD in 50
     do
-        for N in 2 3 4
+        for N in 2 3 4 5 6 8
         do
-            for switch_mode in 'forward' 'early_abort' 'optimistic_abort'
+            for switch_mode in 'read_cache' 'forward' 'early_abort' 'optimistic_abort'
             do
                 experiment_file=$(python gen_experiment.py --stdout-log --sequential-clients \
-                    --name $N"clients" \
-                    -c  "/root/src/tpcc/pytpcc/tpcc.py --config=%e/p4_tpcc.config --no-load --duration 180 --warehouses 1 --stop-on-error --debug --clients $N gotthard" \
+                    -c  "/root/src/tpcc/pytpcc/tpcc.py --config=%e/p4_tpcc.config --no-load --duration 180 --warehouses 1 --stop-on-error --debug --clients %num_clients% gotthard" \
                     --server-cmd  "../pygotthard/store.py -l %l -p %p -r /tmp/tpcc.bin" \
-                    --out-parent $EXPERIMENTS_DIR --mode $switch_mode \
+                    --out-parent $EXPERIMENTS_DIR --mode $switch_mode -p num_clients=$N \
                     --total-delta $TD --delta-ratio $DR)
                 echo $experiment_file
                 experiment_dir=$(dirname $experiment_file)
