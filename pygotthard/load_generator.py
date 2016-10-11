ee12d3b98706b74fd574657f8e3ea9224a901cf6
Theo <theojepsen@g-m-a-i-l-dotcom>
2016-10-11 07:21:35 +0200

    Run load generator for fixed time instead of number of TXNs

--- pygotthard/load_generator.py
+++ pygotthard/load_generator.py
@@ -4,6 +4,7 @@ from threading import Thread
 from gotthard import *
 from random import gauss, randint
 from time import sleep
+import time
 from numpy.random import choice
 import re
 
@@ -15,8 +16,9 @@ class TxnEngine:
     re_tmpl = re.compile('([A-Z]+)\(([^,]+),?\s*([^)]+)?\)')
     re_value = re.compile('^([0-9]+|[a-z])\s*(([+-])\s*([0-9]+|[a-z]))?$')
 
-    def __init__(self, templates, p, count=10):
+    def __init__(self, templates, p, count=None, duration=None):
         assert len(templates) == len(p)
+        assert count is not None or duration is not None
         self.p = p
         self.key_symbols = [] # all the symbols. e.g. [x, y, z]
         self.symbol_to_key = {} # maps a symbol to a concrete key. e.g. x:1, y:2
@@ -24,6 +26,7 @@ class TxnEngine:
         self.symbol_state = {} # the current value for each symbol. e.g. x=23, y=14
         self.max_count = count
         self.count = 0      # number of successfully executed TXNs
+        self.duration = duration # optionally, the max duration in seconds
         self.current_txn = None # currently executing TXN
 
         self.transactions = []
@@ -90,20 +93,25 @@ class TxnEngine:
         self.current_txn = choice(self.transactions, p=self.p)
 
     def getInitTxn(self):
+        if self.duration is not None:
+            self.end_time = time.time() + self.duration
         return [W(key, str(0)) for symbol,key in self.symbol_to_key.items()]
 
     def done(self):
-        return self.count == self.max_count
+        if self.duration is not None:
+            return time.time() > self.end_time
+        else:
+            return self.count == self.max_count
 
 
 class EngineClient(Thread, GotthardClient):
-    def __init__(self, count, log, store_addr, think, think_var, txn_templates, pdf):
+    def __init__(self, count, duration, log, store_addr, think, think_var, txn_templates, pdf):
         GotthardClient.__init__(self, store_addr=store_addr, logger=log)
         Thread.__init__(self)
         self.think = think
         self.think_var = think_var
 
-        self.engine = TxnEngine(txn_templates, pdf, count=count)
+        self.engine = TxnEngine(txn_templates, pdf, count=count, duration=duration)
 
     def run(self):
         if self.think and self.think_var: think_sigma = self.think * self.think_var
@@ -125,17 +133,21 @@ if __name__ == '__main__':
     parser.add_argument("host", type=str, help="server hostname")
     parser.add_argument("port", type=int, help="server port")
     parser.add_argument("--num-clients", "-n", type=int, help="number of parallel clients", default=1)
-    parser.add_argument("--count", "-c", type=int, help="number of transactions to perform", default=1000)
+    parser.add_argument("--count", "-c", type=int, help="number of transactions to perform", default=None)
     parser.add_argument("--log", "-l", type=str, help="filename to write log to", default=None)
     parser.add_argument("--id", "-i", type=int, help="assign cl_id starting from this value", default=None)
     parser.add_argument("--think", "-t", type=float, help="think time (s) between successful transactions", default=None)
     parser.add_argument("--think-var", "-v", type=float, help="variance used for generating random think time", default=None)
+    parser.add_argument("--duration", "-d", type=float, help="Maximum duration of experiment", default=None)
     parser.add_argument("--pdf", "-p", type=lambda s: map(float, s.split(',')), help="Probability of each transaction. Each within [0.0, 1.0]",
             default=[0.85, 0.15])
     parser.add_argument("--transactions", "-T", help="Transactions to execute", required=False,
             type=lambda s: map(lambda t: t.strip(), s.split('|')),
             default=['R(1)', 'A(1, RND) W(1, RND)'])
     args = parser.parse_args()
+
+    if args.count is None and args.duration is None:
+        parser.error("Either --count or --duration is required")
 
     store_addr = (args.host, args.port)
 
@@ -151,7 +163,7 @@ if __name__ == '__main__':
 
     clients = []
     for n in xrange(args.num_clients):
-        cl = EngineClient(args.count, logger, store_addr, args.think, args.think_var, args.transactions, pdf)
+        cl = EngineClient(args.count, args.duration, logger, store_addr, args.think, args.think_var, args.transactions, pdf)
         if not args.id is None: cl.cl_id = args.id + n
         clients.append(cl)
 
