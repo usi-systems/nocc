4b6c6b2f1533bf052fe44d9c87888361392d7a0e
Theo <theojepsen@g-m-a-i-l-dotcom>
2017-02-02 16:35:17 +0100

    Build scripts use P4_16 p4c-bm2-ss

--- switch/env.sh
+++ switch/env.sh
@@ -1,2 +1,3 @@
 BMV2_PATH=/root/p4factory/submodules/bm
 P4C_BM_PATH=/root/p4factory/submodules/p4c-bm
+P4C_PATH=/root/p4c
--- switch/run_experiment.sh
+++ switch/run_experiment.sh
@@ -34,8 +34,8 @@ last_src_change=$(find p4src -exec stat {} --printf="%Y\n" \; | sort -nr | head 
 if [ ! -f $PROG.json ] || [ $(stat -c%Y $PROG.json) -lt $last_src_change ]
 then
     echo "Recompiling P4 program"
-    python p4src/loop_tables.template.py -m 10 > p4src/loop_tables.generated.p4
-    $P4C_BM_SCRIPT p4src/$PROG.p4 --json $PROG.json --p4-v1.1
+    echo $P4C_PATH/build/p4c-bm2-ss p4src/$PROG.p4 -o $PROG.json --p4-16
+    $P4C_PATH/build/p4c-bm2-ss p4src/$PROG.p4 -o $PROG.json --p4-16
 fi
 sudo python experiment.py \
     --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
