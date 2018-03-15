046ee6b3ec47cdf18bc58a662499bf81653b8eda
Theo <theojepsen@g-m-a-i-l-dotcom>
2018-03-15 01:43:30 +0100

    added hooks to runner

--- pygotthard/exp/runner.sh
+++ pygotthard/exp/runner.sh
@@ -9,17 +9,43 @@ if [ $# -lt 1 ]; then
 else
     EXPERIMENTS_DIR=$1
 fi
+export EXPERIMENTS_DIR
+
+if [ ! -d $EXPERIMENTS_DIR ]; then
+    echo "Directory does not exist: $EXPERIMENTS_DIR"
+    exit 1
+fi
+
+EXPERIMENTS_NAME=$(basename $EXPERIMENTS_DIR)
+export EXPERIMENTS_NAME
+
+RESULTS_DIR=results
+export RESULTS_DIR
 
 DONE_DIR="$EXPERIMENTS_DIR/done"
 TORUN_DIR="$EXPERIMENTS_DIR/torun"
 RUNNING_DIR="$EXPERIMENTS_DIR/running"
 mkdir -p $RUNNING_DIR
 
+run_hook() {
+    hook_file=$EXPERIMENTS_DIR/"$1"_hook.sh
+    if [ -x $hook_file ]; then
+        $hook_file
+    fi
+}
+
 while true
 do
 
+    if [ -f $EXPERIMENTS_DIR/pause ]; then
+        echo "Detected pause file. Now exiting. Remove pause file to continue."
+        break
+    fi
+
+    run_hook before
+
     experiment_dirname=$(ls -rt $TORUN_DIR | head -n1)
-    if [ -z $experiment_dirname ] ; then
+    if [ -z $experiment_dirname ]; then
         break
     fi
 
@@ -28,9 +54,14 @@ do
     echo $experiment_dir
     mkdir $experiment_dir/out
 
-    (cd $experiment_dir && time ./run.sh > out/stdout 2> out/stderr)
+    (cd $experiment_dir && time ./run.sh > out/stdout 2> out/stderr) 2>&1 | grep real
+    echo
+
+    run_hook after
 
     sleep 1
+
     mv $experiment_dir $DONE_DIR
+
 done
 
